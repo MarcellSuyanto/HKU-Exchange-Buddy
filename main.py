8b9db99de04508b6ed1076996246a5df5e6402e8
Marcell Suyanto <suyantomarcell@gmail.com>
2026-01-09 00:56:22 +0700

    Update data

--- main.py
+++ main.py
@@ -1,21 +1,40 @@
-from vectorDB.HKUvdb import *
+from vectorDB.vdb import *
 
 CREATE_DB = False
+RELEVANCE_THRESHOLD = 1.00
 
 def main():
     if CREATE_DB:
         create_vector_db()
+    """
+    query->
+    {
+    course_code: The course code of the queried course
+    course_description: The course description of the queried course
+    course_faculty: HKU only, faculty of the course
+    home_university: The original university
+    host_university: The university being visited
+    host_country: The country being visited
+    }
+    """
 
     query = {
-        'course_code' : "CIVL20152",
-        'course_description': "Materials 2 (Civil): The unit builds on and expands the topics of concrete, metals and corrosion which were introduced in Materials 1 (Civil). Additionally, polymer and glass materials are introduced.  In this unit the properties of these materials are illustrated with examples from construction practice.  Deterioration mechanisms (and their prevention) are examined in more detail.",
+        'course_code' : "COMP3314",
+        'course_description': "This course introduces algorithms, tools, practices, and applications of machine learning. Topics include core methods such as supervised learning (classification and regression), unsupervised learning (clustering, principal component analysis), Bayesian estimation, neural networks; common practices in data pre-processing, hyper-parameter tuning, and model evaluation; tools/libraries/APIs such as scikit-learn, Theano/Keras, and multi/many-core CPU/GPU programming.",
+        'home_university': 'HKU'
     }
 
-    results_course_codes, results_course_desc, result_similarity = query_db(query, 3)
+    results_course_codes, results_course_desc, result_similarity = query_db(query, 5)
+    course_counts = 0
     for i in range(len(results_course_codes)):
-        print(results_course_codes[i])
-        print(results_course_desc[i])
-        print(f"Similarity rating: {(1-result_similarity[i])*100:.2f}")
+        if result_similarity[i] < RELEVANCE_THRESHOLD:
+            print(results_course_codes[i])
+            print(results_course_desc[i])
+            print(f"Similarity rating: {(result_similarity[i]):.2f}")
+            course_counts += 1
+    if not course_counts:
+        print("No relevant courses found")
+        
 
 
 if __name__ == "__main__":
